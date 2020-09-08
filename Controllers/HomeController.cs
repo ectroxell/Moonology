1bcd9b1d1a4a8ebca8780dffd3f426912bcf150d
Emma Troxell <troxellemma@gmail.com>
2020-09-07 20:56:43 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Astro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SunCalcNet;
+using SunCalcNet.Model;
+
+namespace Astro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            MoonIllumination moonIllum = MoonCalc.GetMoonIllumination(DateTime.UtcNow);
+
+            MoonData currentMoonData = new MoonData()
+            {
+                Phase = moonIllum.Phase.ToString(),
+                Illumination = moonIllum.Fraction.ToString()
+            }; 
+            
+            
+
+            return View(currentMoonData);
+        }
+
+        
+    }
+}
---
+++ Models/MoonData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Astro.Models
+{
+    public class MoonData
+    {
+        public string Phase { get; set; }
+        public string Illumination { get; set; }
+    }
+}
