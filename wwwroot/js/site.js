96654d82b61290e1ae77b965b8b37b44911ae2ba
Emma Troxell <troxellemma@gmail.com>
2020-09-29 07:31:34 -0500

    implement toggle themes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,23 @@
 ﻿function handleTableRowClick(rowId) {
     document.location = `/Journal/Details/${rowId}`;
 }
+function updateUI() {
+
+    if (localStorage.currentTheme == null) {
+        localStorage.currentTheme = 'bg-pink-clouds';
+    }
+
+    document.documentElement.style.setProperty('--background-image', `url(../imgs/${localStorage.currentTheme}.jpg)`);
+}
+function changeBackground(newBackground) {
+  
+
+    selectedTheme = newBackground === 'blue' ? 'bg-blue-nebula' : 'bg-pink-clouds';
+
+    localStorage.currentTheme = selectedTheme;
+    
+    updateUI();
+}
+
+
+updateUI();
